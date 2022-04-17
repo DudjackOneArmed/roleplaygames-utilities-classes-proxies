4045b68235dee4afcabf0b8791cbb43ff4d70454
Александр Плеханов <Plehalex09@gmail.com>
2022-04-18 00:38:41 +0300

    [REFACTORING] - Added static proxies DI

---
+++ StaticClassesProxies.System.DependencyInjection.Microsoft/IServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace StaticClassesProxies.System.DependencyInjection.Microsoft
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddStaticClassesProxies(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IDirectory, DirectoryImpl>();
+            serviceCollection.AddTransient<IFile, FileImpl>();
+
+            return serviceCollection;
+        }
+    }
+}
