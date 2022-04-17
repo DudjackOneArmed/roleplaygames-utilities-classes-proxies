24d47b191e86bd45711baf337e9668b6a070f050
Александр Плеханов <Plehalex09@gmail.com>
2022-04-18 01:01:52 +0300

    [COM-51] - DateTimeService was added

---
+++ StaticClassesProxies.System/DateTimeService/DateTimeService.cs
@@ -0,0 +1,15 @@
+﻿namespace System
+{
+    /// <inheritdoc/>
+    public class DateTimeService : IDateTimeService
+    {
+        /// <inheritdoc/>
+        public DateTime Now => DateTime.Now;
+
+        /// <inheritdoc/>
+        public DateTime UtcNow => DateTime.UtcNow;
+
+        /// <inheritdoc/>
+        public DateTime Today => DateTime.Today;
+    }
+}
